266236e8ced02446128fcca383fc5ee8f7e55710
Madhu Sri Alapani <madhusrialapani@gmail.com>
2025-09-03 17:48:06 +0530

    Official Dashboard in progress

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage";
 import SignupPage from "./Pages/SignupPage";
 import LandingPage from "./Pages/LandingPage";
+import OfficialDashboard from "./Pages/OfficialDashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/OfficialDashboard" element={<OfficialDashboard />} />
       </Routes>
     </Router>
   );
